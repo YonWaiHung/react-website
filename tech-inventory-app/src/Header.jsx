2578b8dd638b0c40d4645a30cb9449e4bffff86b
YonWaiHung <waihung.yon@beans.com.my>
2024-05-13 17:58:17 +0800

    title change & more CSS styling

--- tech-inventory-app/src/Header.jsx
+++ tech-inventory-app/src/Header.jsx
@@ -1,21 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <h2>MyApp</h2>
+        <h2>React Web 4 Tech Item Management</h2>
       </div>
-      <nav className="desktop-nav">
+      {/* <nav className="desktop-nav">
         <ul>
           <li><a href="/">Home</a></li>
           <li><a href="/">List</a></li>
           <li><a href="/">Add</a></li>
           <li><a href="/">Settings</a></li>
         </ul>
-      </nav>
+      </nav> */}
     </header>
   )
 
